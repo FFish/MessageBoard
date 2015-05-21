d7cc5ffb2c8eb2777997a5e78970dd4d30764280
unknown <ynz1991@gmail.com>
2015-05-21 20:37:05 +0800

    finish insert and select of message

--- msgstart.py
+++ msgstart.py
@@ -1,35 +1,52 @@
-from flask import Flask, render_template, request, g
 import sqlite3
+
+from flask import Flask, render_template, request, g, redirect, url_for
+
 
 
 # Configuration
-DATABASE = "/db/message.db"
+DATABASE = "db/message.db"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def contect_db():
-    return sqlite3.connect(app.config['DATABASE']) 
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 @app.before_request
 def before_request():
-    g.db = contect_db()
+    g.db = connect_db()
+
 
 @app.teardown_request
-def teardown_request():
+def teardown_request(exception):
+    if exception is not None:
+        print exception
     db = getattr(g, 'db', None)
     if db is not None:
-	db.close()
+        db.close()
+
 
 @app.route("/add", methods=['POST'])
-def add(): 
-    g.db.execute('insert into entries (title, content) values (?,?)', (request.form['title'], request.form['content']))
+def add():
+    g.db.execute('insert into entries (title, text) values (?,?)', (request.form['title'], request.form['content']))
     g.db.commit()
-    return "Add Successfully" 
+    return redirect(url_for("index"))
+
+
+@app.route("/index")
+def index():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template("index.html", entries=entries)
+
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def root():
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run()
